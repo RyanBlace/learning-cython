5f7a21f48d89349a94d6e7e01b355cb3a6965280
Ryan Blace <ryan.blace@levelplane.ai>
2024-12-23 14:34:29 +0800

    Working example

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cython
+
+## Links
+
+* <https://github.com/cython/cython>
+* <https://github.com/cython/cython/wiki>
+* <https://cython.readthedocs.io/en/latest/index.html>
---
+++ test_so_library.py
@@ -0,0 +1,9 @@
+import logic
+
+def main():
+    result = logic.add_numbers(1, 2)
+    print("Over 6100 lines of C added 1 to 2 and got:", result)
+
+
+if __name__ == "__main__":
+    main()
